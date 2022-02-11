6f5a16c37776ab6feea53818af17f10d54c88d73
unknown <tgantara@gmail.com>
2022-02-11 22:42:56 +0700

    update error hak akses

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,42 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import '../Sidebar/Sidebar.css';
 import { NavContext } from '../../contexts/NavContext';
 function Sidebar() {
   const { isNav, none, visible } = useContext(NavContext)
   const nav = isNav ? visible : none;
+  const [active, setActive] = useState('')
+  const [sidebar, setSidebar] = useState([
+    {
+      name: 'Dashboard',
+      link: '',
+      icon: <ion-icon name="speedometer"></ion-icon>
+    },
+    {
+      name: 'Dashboard2',
+      link: '',
+      icon: <ion-icon name="speedometer"></ion-icon>
+    }
+  ])
+  const actionSetActive = (name) => {
+    setActive(name)
+  }
   return (
     <div className='body-sidebar' style={{ display: nav.css }}>
-      <div>
-        <i>
-          <ion-icon name="speedometer"></ion-icon>
-        </i>
-        <p>lorem</p>
-      </div>
+      {
+        sidebar.map(sidebar => {
+          return (
+            <div className={`list-sidebar ${active == sidebar.name ? 'active' : ''}`} onClick={() => actionSetActive(sidebar.name)}>
+              <i>
+                {sidebar.icon}
+              </i>
+              <p>
+                {sidebar.name}
+              </p>
+            </div>
+          )
+        })
+      }
+
     </div>
   )
 }
