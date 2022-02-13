4ab6e0bb52e41ad3ea4dd863110583ceb77cb598
unknown <tgantara@gmail.com>
2022-02-13 20:52:36 +0700

    create dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import './index.css'
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className="body-dashboard">
+      <div className="left-dashboard">
+        <div className="left-dashboard-header">
+          <h1>Dashboard</h1>
+          <select name="" id="">
+            <option value="">This Week</option>
+          </select>
+        </div>
+      </div>
+      <div className='right-dashboard'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet exercitationem maiores aliquam iusto soluta autem sit animi voluptate dignissimos consequatur.
+      </div>
+    </div>
   )
 }
 
